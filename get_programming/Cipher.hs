9838165f81072aaef906753bced5ab7f2264a10c
sharofiddin <pardayev.sharofiddin@gmail.com>
2026-04-10 15:37:59 +0500

    finished stream cipher

--- get_programming/Cipher.hs
+++ get_programming/Cipher.hs
@@ -89,3 +89,37 @@ instance Cipher OneTimePad where
    encode (OTP pad) = applyOTP pad
    decode (OTP pad) = applyOTP pad
 
+prng :: Int -> Int -> Int -> Int -> Int
+prng a b maxNumber seed = (a * seed + b) `mod` maxNumber
+
+myPrng :: Int -> Int
+myPrng = prng 1337 7 100
+
+randIntStream :: Int -> [Int]
+randIntStream seed = seed : randIntStream (myPrng seed)
+
+randBitStream :: [Int] -> Bits
+randBitStream values = concat (map intToBits  values)
+
+streamEncode :: Int -> String -> String
+streamEncode seed text = applyOTP pad text
+     where padInt = take (length text) (randIntStream seed)
+           padBits = map  intToBits padInt
+           pad = map bitsToChar padBits 
+
+data StreamCipher = StreamCipher Int
+
+instance Cipher StreamCipher where 
+  encode (StreamCipher seed) = streamEncode seed
+  decode (StreamCipher seed) = streamEncode seed
+-- ghci> encode (StreamCipher 100) "salom"
+-- "\ETBf.^y"
+-- ghci> encode (StreamCipher 95) "salom"
+-- ",wy;b"
+-- ghci> encode (StreamCipher 95) "salom do'stim qalaysan ahvollaring yaxshimi?"
+-- ",wy;b\RSeC\EOTq%mZ\nL!'3Pk>x55gHn@Oc#mYM\GS9**Zp6{|k"
+-- ghci> cipherText = encode (StreamCipher 95) "salom do'stim qalaysan ahvollaring yaxshimi?"
+-- ghci> cipherText
+-- ",wy;b\RSeC\EOTq%mZ\nL!'3Pk>x55gHn@Oc#mYM\GS9**Zp6{|k"
+-- ghci> decode (StreamCipher 95) cipherText
+-- "salom do'stim qalaysan ahvollaring yaxshimi?"

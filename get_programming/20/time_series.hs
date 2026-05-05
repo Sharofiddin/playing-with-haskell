c72129c4ba4d5229ce36af9a2c634d4fb50005a1
sharof <pardaev.sharofiddin@technobiz.sg>
2026-05-05 18:36:32 +0500

    ts

--- get_programming/20/time_series.hs
+++ get_programming/20/time_series.hs
@@ -1,8 +1,11 @@
+import Data.ByteString (count)
 import Data.List
 import Data.Map qualified as Map
 import Data.Maybe
 import Data.Semigroup
-import Text.XHtml (rows)
+import Distribution.Simple (VersionInterval)
+import GHC.IO.Encoding (argvEncoding)
+import Text.XHtml (rows, value)
 
 file1 :: [(Int, Double)]
 file1 =
@@ -70,7 +73,7 @@ fileToTS tvPairs = createTS times values
   where
     (times, values) = unzip tvPairs
 
-showTVPair :: (Show a) => Int -> (Maybe a) -> String
+showTVPair :: (Show a) => Int -> Maybe a -> String
 showTVPair time (Just value) = mconcat [show time, "|", show value, "\n"]
 showTVPair time Nothing = mconcat [show time, "|N/A\n"]
 
@@ -90,3 +93,76 @@ ts3 = fileToTS file3
 
 ts4 :: TS Double
 ts4 = fileToTS file4
+
+insertMaybePair :: (Ord k) => Map.Map k v -> (k, Maybe v) -> Map.Map k v
+insertMaybePair myMap (_, Nothing) = myMap
+insertMaybePair myMap (k, Just v) = Map.insert k v myMap
+
+combineTS :: TS a -> TS a -> TS a
+combineTS (TS [] []) ts2 = ts2
+combineTS ts1 (TS [] []) = ts1
+combineTS (TS t1 v1) (TS t2 v2) = TS completeTimes combinedValues
+  where
+    bothTimes = mconcat [t1, t2]
+    completeTimes = [minimum bothTimes .. maximum bothTimes]
+    tvMap = foldl insertMaybePair Map.empty (zip t1 v1)
+    updatedMap = foldl insertMaybePair tvMap (zip t2 v2)
+    combinedValues = map (\v -> Map.lookup v updatedMap) completeTimes
+
+instance Semigroup (TS a) where
+  (<>) = combineTS
+
+instance Monoid (TS a) where
+  mempty = TS [] []
+  mappend = (<>)
+
+tsAll :: TS Double
+tsAll = mconcat [ts1, ts2, ts3, ts4]
+
+mean :: (Real a) => [a] -> Double
+mean xs = total / count
+  where
+    total = (realToFrac . sum) xs
+    count = (realToFrac . length) xs
+
+meanTS :: (Real a) => TS a -> Maybe Double
+meanTS (TS _ []) = Nothing
+meanTS (TS times values) =
+  if all (== Nothing) values
+    then Nothing
+    else Just avg
+  where
+    justVals = filter isJust values
+    cleanVals = map fromJust justVals
+    avg = mean cleanVals
+
+type CompareFunc a = a -> a -> a
+
+type TSCompareFunc a = (Int, Maybe a) -> (Int, Maybe a) -> (Int, Maybe a)
+
+makeTSCompare :: (Eq a) => CompareFunc a -> TSCompareFunc a
+makeTSCompare func = newFunc
+  where
+    newFunc (i1, Nothing) (i2, Nothing) = (i1, Nothing)
+    newFunc (_, Nothing) (i2, val) = (i2, val)
+    newFunc (i1, val) (i2, Nothing) = (i1, val)
+    newFunc (i1, Just val1) (i2, Just val2) =
+      if func val1 val2 == val1
+        then (i1, Just val1)
+        else (i2, Just val2)
+
+compareTS :: (Eq a) => (a -> a -> a) -> TS a -> Maybe (Int, Maybe a)
+compareTS func (TS [] []) = Nothing
+compareTS func (TS keys values) =
+  if all (== Nothing) values
+    then Nothing
+    else Just best
+  where
+    pairs = zip keys values
+    best = foldl (makeTSCompare func) (0, Nothing) pairs
+
+maxTS :: (Ord a) => TS a -> Maybe (Int, Maybe a)
+maxTS = compareTS max
+
+minTS :: (Ord a) => TS a -> Maybe (Int, Maybe a)
+minTS = compareTS min

6f087c94aa214337f4f6cd07b9f0dd2484ca3182
sharof <pardaev.sharofiddin@technobiz.sg>
2026-06-02 19:08:35 +0500

    capstone

--- get_programming/33/hinq.hs
+++ get_programming/33/hinq.hs
@@ -1,3 +1,5 @@
+import Control.Monad
+
 data Name = Name
   { firstName :: String,
     secondName :: String
@@ -29,3 +31,50 @@ students =
     Student 5 Sophmore (Name "Jean" "Baudrillard"),
     Student 6 Junior (Name "Julia" "Kristeva")
   ]
+
+_select :: (a -> b) -> [a] -> [b]
+_select prop vals = do
+  val <- vals
+  return (prop val)
+
+_where :: (a -> Bool) -> [a] -> [a]
+_where test vals = do
+  val <- vals
+  guard (test val)
+  return val
+
+startsWith :: Char -> String -> Bool
+startsWith char string = char == head string
+
+data Teacher = Teacher
+  { teacherId :: Int,
+    teacherName :: Name
+  }
+  deriving (Show)
+
+data Course = Course
+  { courseId :: Int,
+    courseTitle :: String,
+    teacher :: Int
+  }
+  deriving (Show)
+
+teachers :: [Teacher]
+teachers =
+  [ Teacher 100 (Name "Simone" "De Beauvior"),
+    Teacher 200 (Name "Susan" "Sontag")
+  ]
+
+courses :: [Course]
+courses =
+  [ Course 101 "French" 100,
+    Course 201 "English" 200
+  ]
+
+_join :: (Eq c) => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a, b)]
+_join data1 data2 prop1 prop2 = do
+  d1 <- data1
+  d2 <- data2
+  let dpairs = (d1, d2)
+  guard (prop1 (fst dpairs) == prop2 (snd dpairs))
+  return dpairs
